9e44628443ca9f20a957f55479f44f8cdb300a62
Nyapal <nyapal14@gmail.com>
2020-10-21 11:53:58 -0700

    added grid board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import GridBoard from './components/GridBoard';
 import GridSquare from './components/GridSquare';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <h1 className="App-title"> Tetris Redux </h1>
       </header>
-      <GridSquare color="1" />
+      <GridBoard />
     </div>
   );
 }
