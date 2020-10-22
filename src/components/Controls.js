95680d4c73675d9973cbdb09efd9bee87260e51a
Nyapal <nyapal14@gmail.com>
2020-10-21 22:53:41 -0700

    added controls

---
+++ src/components/Controls.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Controls(props) {
+  return (
+    <div>
+      <button className="control-button" onClick={(e) => {}}> Left </button>
+      <button className="control-button" onClick={(e) => {}}> Right </button>
+      <button className="control-button" onClick={(e) => {}}> Rotate </button>
+      <button className="control-button" onClick={(e) => {}}> Down </button>
+    </div>
+  )
+}
